1a4df9d9ae4ddabcedba617a5e3fd521bb927705
Ralf Prezia <ralfprezia2gmail.com>
2021-09-09 11:43:10 -0300

    Add project files.

---
+++ Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraTests
+    {
+       [TestFixture]
+       public class CalculadoraSimplesTests
+        {
+            [Test]
+            public void DevoAdicionarDoisNumeros()
+            {
+                //SUT
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Adicionar(5, 5);
+
+                Assert.That(resultado, Is.EqualTo(10));
+            } 
+
+            [Test]
+            public void DevoMultiplicarDoisNumeros()
+            {
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Multiplicar(2, 4);
+
+                Assert.That(resultado, Is.EqualTo(8));
+            }
+        }
+    }
+}
---
+++ Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            //bug para fins demonstrativos (bug) => return num1 * num2
+            return num1 * num2;
+        }
+    }
+}
